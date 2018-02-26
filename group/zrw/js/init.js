a1246a7b477c5058691fbe341625ff7d89952cdf
chengjun <wangchj04@gmail.com>
2018-02-26 20:16:37 +0800

    zrw

---
+++ group/zrw/js/init.js
@@ -0,0 +1,53 @@
+
+
+skel.init({
+	prefix: 'css/style',
+	resetCSS: true,
+	boxModel: 'border',
+	grid: {
+		gutters: 50
+	},
+	breakpoints: {
+		'mobile': {
+			range: '-480',
+			lockViewport: true,
+			containers: 'fluid',
+			grid: {
+				collapse: true,
+				gutters: 10
+			}
+		},
+		'desktop': {
+			range: '481-',
+			containers: 1200
+		},
+		'1000px': {
+			range: '481-1200',
+			containers: 960
+		}
+	}
+}, {
+	panels: {
+		panels: {
+			navPanel: {
+				breakpoints: 'mobile',
+				position: 'left',
+				style: 'reveal',
+				size: '80%',
+				html: '<div data-action="navList" data-args="nav"></div>'
+			}
+		},
+		overlays: {
+			titleBar: {
+				breakpoints: 'mobile',
+				position: 'top-left',
+				height: 44,
+				width: '100%',
+				html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span>' +
+ '<span class="title" data-action="copyHTML" data-args="logo"></span>'
+			}
+		}
+	}
+	
+	
+});
